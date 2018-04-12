30d5244ebe4709516ec202a25303effe03a5b027
nikischultz14 <nikischultz14@gmail.com>
2018-04-12 11:15:44 -0500

    update 2

---
+++ src/test/java/com/nikischultz/firstprojectspring/FirstProjectSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nikischultz.firstprojectspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstProjectSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
